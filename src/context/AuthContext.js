7f3168221aa0554addb01a2daa5168b1ca3cd428
NidhitheCoder <nidhpv@gmail.com>
2020-12-07 21:41:10 +0530

    Sign in completed

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,13 +1,13 @@
 import { AsyncStorage } from "react-native";
 import createDataContext from "./createDataContext";
 import trackerApi from "../api/tracker";
-import {navigate} from '../navigationRef';
+import { navigate } from "../navigationRef";
 
 const AuthReducer = (state, action) => {
   switch (action.type) {
     case "add_error":
       return { ...state, errorMessage: action.payload };
-    case "signup":
+    case "signin":
       return {
         ...state,
         errorMessage: "",
@@ -22,9 +22,9 @@ const signup = dispatch => async ({ email, password }) => {
   try {
     const response = await trackerApi.post("/signup", { email, password });
     await AsyncStorage.setItem("token", response.data.token);
-    dispatch({ type: signup, payload: response.data.token });
+    dispatch({ type: "signin", payload: response.data.token });
     // navigate to main flow
-    navigate('TrackList');
+    navigate("TrackList");
   } catch (err) {
     dispatch({
       type: "add_error",
@@ -33,12 +33,18 @@ const signup = dispatch => async ({ email, password }) => {
   }
 };
 
-const signin = dispatch => {
-  return ({ email, password }) => {
-    // Trying to sign in.
-    // Handle by success by updating state.
-    // Handle failure by showing error message someware.
-  };
+const signin = dispatch => async ({ email, password }) => {
+  try {
+    const response = await trackerApi.post("/signin", { email, password });
+    await AsyncStorage.setItem("token", response.data.token);
+    dispatch({ type: "signin", payload: response.data.token });
+    navigate("TrackList");
+  } catch (err) {
+    dispatch({
+      type: "add_error",
+      payload: "Something went wrong with sign in"
+    });
+  }
 };
 
 const signout = dispatch => {
--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React,{useContext}from "react";
 import { View, StyleSheet, Text } from "react-native";
 import AuthForm from '../components/AuthForm';
 import NavLink from '../components/NavLink';
+import {Context as AuthContext} from '../context/AuthContext';
 
 const SignInScreen = () => {
+  const {state,signin} = useContext(AuthContext);
   return (
     <View style={styles.containerStyle}>
     <AuthForm
     headerText="Sign in to Your Account"
-    errorMessage=""
-    onSubmit={()=>{}}
+    errorMessage={state.errorMessage}
+    onSubmit={signin}
     submitButtonText="Sign In"
     />
     <NavLink
