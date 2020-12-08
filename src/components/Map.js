dc24e4d35fcefa3e363b106bbf2c87a34d5fa549
NidhitheCoder <nidhpv@gmail.com>
2020-12-08 15:27:10 +0530

    Add react native map to our app and set initial region

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import {Text,StyleSheet} from 'react-native';
+import {StyleSheet} from 'react-native';
+import MapView from 'react-native-maps'
 
 const Map =() => {
-    return <Text>Map</Text>;
+    return <MapView
+    initialRegion={{
+         latitude:11.6629,
+         longitude:76.2570,
+         latitudeDelta:0.01,
+         longitudeDelta:0.01
+    }}
+     style={styles.mapStyle}
+     />
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    mapStyle:{
+        height:300
+    }
+});
  
 export default Map;
