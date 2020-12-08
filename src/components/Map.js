3c512f19bf6cea856e260691932d388d667ca067
NidhitheCoder <nidhpv@gmail.com>
2020-12-08 15:39:11 +0530

    Drowing a series of lines

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,23 +1,36 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
-import MapView from 'react-native-maps'
+import React from "react";
+import { StyleSheet } from "react-native";
+import MapView, { Polyline } from "react-native-maps";
 
-const Map =() => {
-    return <MapView
-    initialRegion={{
-         latitude:11.6629,
-         longitude:76.2570,
-         latitudeDelta:0.01,
-         longitudeDelta:0.01
-    }}
-     style={styles.mapStyle}
-     />
+const Map = () => {
+  let points = [];
+
+  for (let i = 0; i < 20; i++) {
+    points.push({
+      latitude: 11.6629 + i * 0.001,
+      longitude: 76.257 + i * 0.001
+    });
+  }
+
+  return (
+    <MapView
+      initialRegion={{
+        latitude: 11.6629,
+        longitude: 76.257,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+      }}
+      style={styles.mapStyle}
+    >
+      <Polyline coordinates={points} />
+    </MapView>
+  );
 };
 
 const styles = StyleSheet.create({
-    mapStyle:{
-        height:300
-    }
+  mapStyle: {
+    height: 300
+  }
 });
- 
+
 export default Map;
