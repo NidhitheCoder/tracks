94b1794a9e21f633762e090ebcbdfbf472a151cf
NidhitheCoder <nidhpv@gmail.com>
2020-12-09 17:54:29 +0530

    effective use of useEffect

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -7,40 +7,42 @@ import {
 
 export default (shouldTrack, callback) => {
   const [err, setErr] = useState(null);
-  const [subscriber, setSubscriber] = useState(null);
-
-  const startWatching = async () => {
-    try {
-      await requestPermissionsAsync();
-      const sub = await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000,
-          distanceInterval: 10
-        },
-        callback
-      );
-      setSubscriber(sub);
-    } catch (err) {
-      setErr(err);
-    }
-  };
+  // const [subscriber, setSubscriber] = useState(null);
 
   useEffect(() => {
+    let subscriber;
+    const startWatching = async () => {
+      try {
+        await requestPermissionsAsync();
+        subscriber = await watchPositionAsync(
+          {
+            accuracy: Accuracy.BestForNavigation,
+            timeInterval: 1000,
+            distanceInterval: 10
+          },
+          callback
+        );
+      } catch (err) {
+        setErr(err);
+      }
+    };
+
     if (shouldTrack) {
       startWatching();
     } else {
       // stop watching
-      subscriber.remove();
-      setSubscriber(null);
+      if (subscriber) {
+        subscriber.remove();
+      }
+      subscriber = null;
     }
 
-    return()=>{
-        if(subscriber) {
-            subscriber.remove();
-        }
+    return () => {
+      if (subscriber) {
+        subscriber.remove();
+      }
     };
-  }, [shouldTrack,callback]);
+  }, [shouldTrack, callback]);
 
   return [err];
 };
