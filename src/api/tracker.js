bf27be81eb345f14336219d0d04e6c6bca255b01
NidhitheCoder <nidhpv@gmail.com>
2021-01-26 09:17:48 +0530

    Authomatic authorization

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,22 @@
 import axios from 'axios';
+import {AsyncStorage} from 'react-native';
 
-export default axios.create({
-    baseURL:'http://0d9a41b5fe6e.ngrok.io'
-})
+const instance = axios.create({
+    baseURL:'http://d4a41b406e53.ngrok.io'
+});
+
+
+// first function inside in interceptors is for make request and 
+// second one is when we get an error withour making request
+instance.interceptors.request.use(
+   async (config) =>{
+       const token = await AsyncStorage.getItem('token');
+       if(token){
+           config.headers.Authorization = `Bearer ${token}`;
+       }
+       return config;
+   },
+    (err) => Promise.reject(err)
+);
+
+export default instance;
